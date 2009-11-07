2c12b9a47c20c4a5fea9e84b43b4a0840b33ed06
Andrew Stone <andy@stonean.com>
2009-11-06 19:00:02 -0500

    regex test

--- spec/ruhl_spec.rb
+++ spec/ruhl_spec.rb
@@ -335,9 +335,8 @@ describe Ruhl do
 
     it "will convert entities" do
       ps = @doc.xpath("/html/body/p")
-      ps.to_s.should == "<p>Here is a space&nbsp;and another&nbsp;one.</p><p>RuHL &copy; 2009</p>"
-      #ps[0].inner_html.should == "Here is a space&nbsp;and another&nbsp;one."
-      #ps[1].inner_html.should == "RuHL &copy; 2009"
+      ps[0].inner_html.should =~ /^Here is a space&nbsp;and another&nbsp;one.$/
+      ps[1].inner_html.should == "RuHL &copy; 2009"
 
       # To verify everything is correct, I did it the old fashioned way
       # File.open('test.html',"w") do |f|
