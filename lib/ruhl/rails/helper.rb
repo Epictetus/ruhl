197334e64cc3b999d0d5c4fd76c2a81353f3a97d
Andrew Stone <andy@stonean.com>
2009-11-21 16:28:13 -0500

    simplify input field generation

--- lib/ruhl/rails/helper.rb
+++ lib/ruhl/rails/helper.rb
@@ -4,6 +4,15 @@ module Ruhl
       def form_authenticity
         {:value => form_authenticity_token, :type => "hidden", :name => "authenticity_token"}
       end  
+
+      def i(type,column)
+        model = presentee.class.name.underscore
+        { :type => type,
+          :id   => "#{model}_#{column}", 
+          :name => "#{model}[#{column}]",
+          :value => presentee.send(column)
+        }
+      end
     end
   end
 end
