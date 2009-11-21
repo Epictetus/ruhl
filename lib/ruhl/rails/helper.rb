30b5cc12934eed64070f1b726fafeea1f4d57910
Andrew Stone <andy@stonean.com>
2009-11-21 11:41:33 -0500

    helps to spell authenticity correctly

--- lib/ruhl/rails/helper.rb
+++ lib/ruhl/rails/helper.rb
@@ -2,7 +2,7 @@ module Ruhl
   module Rails
     module Helper
       def form_authenticity
-        {:value => form_autheticity_token, :type => "hidden", :name => "authenticity_token"}
+        {:value => form_authenticity_token, :type => "hidden", :name => "authenticity_token"}
       end  
     end
   end
