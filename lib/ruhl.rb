75a89fb935bf4c724327ec7247e6b2d47ea3270c
stonean <andy@stonean.com>
2010-06-15 10:32:26 -0400

    remove orphaned variables

--- lib/ruhl.rb
+++ lib/ruhl.rb
@@ -8,18 +8,12 @@ require 'ruhl/errors'
 module Ruhl
   class << self
     attr_accessor :logger, :encoding
-    attr_accessor :inspect_local_object, :inspect_block_object
-    attr_accessor :inspect_scope
     attr_accessor :use_instance_variables, :log_instance_variable_warning
   end
 
   self.logger = Logger.new(STDOUT)
 
   self.encoding = 'UTF-8'
-
-  self.inspect_local_object = false
-  self.inspect_block_object = false
-  self.inspect_scope = false
 
   self.use_instance_variables = true
   self.log_instance_variable_warning = true
